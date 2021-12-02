203325100ec687490bb35bf4003568050bca0f78
贺妍 <heyandyx123456@163.com>
2021-12-02 11:45:06 +0800

    line2

--- lineDivision_part2/lineNotification.m
+++ lineDivision_part2/lineNotification.m
@@ -1,31 +1,15 @@
 function nums = lineNotification(Im)
 clc
-%Im = imread('E:\奇奇怪怪的作业\tmp2.png');
+Im = imread('E:\奇奇怪怪的作业\tmp3.jpg');
 Im = rgb2gray(Im);
 threshold = graythresh(Im);
-bw = im2bw(Im, threshold);
+bw = im2bw(Im, 0.7);
 [m,n] = size(bw);
-for i = 2:m-1
-   for j = 2:n-1
-       %同上下元素判断       
-       if(bw(i,j)~=bw(i+1,j) && bw(i,j)~=bw(i-1,j))
-           bw(i,j) = 1;
-       %同左右元素判断
-       elseif(bw(i,j)~=bw(i,j+1) && bw(i,j)~=bw(i,j-1))
-           bw(i,j) = 1;
-       %同斜边元素判断
-       elseif(bw(i,j)~=bw(i+1,j+1) && bw(i,j)~=bw(i-1,j-1))
-           bw(i,j) = 1;
-       %同斜边元素判断
-       elseif(bw(i,j)~=bw(i-1,j+1) && bw(i,j)~=bw(i+1,j-1))
-           bw(i,j) = 1;
-       end
-   end
-end
 bw = edge(bw, 'canny');
-[H, theta, rho] = hough(bw, 'Theta', -90:0.1:89);
-p = houghpeaks(H, 16,'threshold', ceil(0.5*max(H(:))));
-lines = houghlines(bw,theta,rho,p,'FillGap',100,'MinLength',20);
+[H, theta, rho] = hough(bw, 'RhoResolution', 1.5, 'Theta', -90:0.1:89);
+p = houghpeaks(H, 20,'threshold', ceil(0.5*max(H(:))));
+lines = houghlines(bw,theta,rho,p,'FillGap',300,'MinLength',10);
+
 
 
 i = 1;
@@ -46,19 +30,49 @@ for k = 1 : length(lines)
    end
 end
 if length(xAddr1) < 16
-    xAddr = cat(2, xAddr1, xAddr2);
+    if length(xAddr1) == 8
+        xAddr = cat(2, xAddr1, xAddr2);
+        xAddr(17) = 0;
+        xAddr(18) = m;
+    else
+        xAddr1 = sort(xAddr1);
+        xAddr1 = xAddr1(2:10);
+        xAddr2 = sort(xAddr2);
+        xAddr2 = xAddr2(1:9);
+        xAddr = cat(2, xAddr1, xAddr2);
+    end
 else
-    xAddr = xAddr1;
+    if length(xAddr1) == 16
+        xAddr = xAddr1;
+        xAddr(17) = 0;
+        xAddr(18) = m;
+    else
+        xAddr1 = sort(xAddr1);
+        xAddr = xAddr1(2:19);
+    end
 end
 if length(yAddr1) < 16
-    yAddr = cat(2, yAddr1, yAddr2);
+    if length(yAddr1) == 8
+        yAddr = cat(2, yAddr1, yAddr2);
+        yAddr(17) = 0;
+        yAddr(18) = n;
+    else
+        yAddr1 = sort(yAddr1);
+        yAddr1 = yAddr1(2:10);
+        yAddr2 = sort(yAddr2);
+        yAddr2 = yAddr2(1:9);
+        yAddr = cat(2, yAddr1, yAddr2);
+    end
 else
-    yAddr = yAddr1;
+    if length(yAddr1) == 16
+        yAddr = yAddr1;
+        yAddr(17) = 0;
+        yAddr(18) = n;
+    else
+        yAddr1 = sort(yAddr1);
+        yAddr = yAddr1(2:19);
+    end
 end
-yAddr(17) = 0;
-yAddr(18) = n;
-xAddr(17) = 0;
-xAddr(18) = m;
 xAddr = sort(xAddr);
 yAddr = sort(yAddr);
 disp(xAddr)
@@ -68,8 +82,28 @@ j1=1; j2=2;
 count = 1;
 nums = zeros;
 for k = 1 : 81
-    %subplot(9,9,count), imshow(Im(xAddr(i1)+1:xAddr(i2)-1, yAddr(j1)+1:yAddr(j2)-1));
-    nums(count) = Im(xAddr(i1)+1:xAddr(i2)-1, yAddr(j1)+1:yAddr(j2)-1);
+    if i1==1
+        ti1 = i1;
+    else
+        ti1 = i1 - 1;
+    end
+    if j1==1
+        tj1 = j1;
+    else
+        tj1 = j1 - 1;
+    end
+    if i2==18
+        ti2 = i2;
+    else
+        ti2 = i2 + 1;
+    end
+    if j2==18
+        tj2 = j2;
+    else
+        tj2 = j2 + 1;
+    end
+    %subplot(9,9,count), imshow(Im(xAddr(ti1)+1:xAddr(ti2)-1, yAddr(tj1)+1:yAddr(tj2)-1));
+    nums(count) = Im(xAddr(ti1)+1:xAddr(ti2)-1, yAddr(tj1)+1:yAddr(tj2)-1);
     j1=j1+2; j2=j2+2;
     count = count + 1;
     if j1==19
